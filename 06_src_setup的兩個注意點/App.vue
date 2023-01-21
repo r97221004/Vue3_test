129748e1563d890c998170d4d1b185d0707f0bce
r97221004 <r97221004@ntu.edu.tw>
2023-01-21 21:42:54 +0800

    add mod06

---
+++ 06_src_setup的兩個注意點/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Demo @hello="showHelloMsg" msg="你好啊" school="尚矽谷">
+    <template v-slot:qwe>
+      <span>尚矽谷</span>
+    </template>
+    <template v-slot:asd>
+      <span>尚矽谷</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+import Demo from "./components/Demo";
+export default {
+  name: "App",
+  components: { Demo },
+  setup() {
+    function showHelloMsg(value) {
+      alert(`你好啊，你觸發了hello事件，我收到的參數是:${value}！`);
+    }
+    return {
+      showHelloMsg,
+    };
+  },
+};
+</script>
---
+++ 06_src_setup的兩個注意點/components/Demo.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>一個人的信息</h1>
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年齡：{{ person.age }}</h2>
+  <button @click="test">測試觸發一下 Demo 組件的 Hello 事件</button>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "Demo",
+  props: ["msg", "school"],
+  emits: ["hello"],
+  setup(props, context) {
+    // console.log("---setup---", props);
+    // console.log("---setup---", context);
+    // console.log('---setup---',context.attrs) //相當與 Vue2 中的 $attrs
+    // console.log('---setup---',context.emit) //觸發自定義事件的。
+    console.log("---setup---", context.slots); //插槽
+    //數據
+    let person = reactive({
+      name: "張三",
+      age: 18,
+    });
+
+    //方法
+    function test() {
+      context.emit("hello", 666);
+    }
+
+    //返回一個對象（常用）
+    return {
+      person,
+      test,
+    };
+  },
+};
+</script>
