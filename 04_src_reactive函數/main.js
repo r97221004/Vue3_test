9444dab32444748536096ae10c388fa3a40822c3
r97221004 <r97221004@ntu.edu.tw>
2023-01-21 15:38:21 +0800

    add mod04

---
+++ 04_src_reactive函數/main.js
@@ -0,0 +1,9 @@
+//引入的不再是 Vue 構造函數了，引入的是一個名為 createApp 的工廠函數
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//創建應用實例對象—— app(類似於之前Vue2中的 vm，但 app 比 vm 更“輕”)
+const app = createApp(App)
+
+//掛載
+app.mount('#app')
