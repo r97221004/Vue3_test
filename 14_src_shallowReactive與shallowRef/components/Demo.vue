3c0bb3f826ea8cfddb288eae9f9cab28aad8569b
r97221004 <r97221004@ntu.edu.tw>
2023-01-23 15:23:41 +0800

    add mod14

---
+++ 14_src_shallowReactive與shallowRef/components/Demo.vue
@@ -0,0 +1,44 @@
+<template>
+  <h4>當前的x.y值是：{{ x.y }}</h4>
+  <button @click="x = { y: 888 }">點我替換x</button>
+  <button @click="x.y++">點我x.y++</button>
+  <hr />
+  <h4>{{ person }}</h4>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年齡：{{ age }}</h2>
+  <h2>薪資：{{ job.j1.salary }}K</h2>
+  <button @click="name += '~'">修改姓名</button>
+  <button @click="age++">增長年齡</button>
+  <button @click="job.j1.salary++">漲薪</button>
+</template>
+
+<script>
+import { ref, reactive, toRef, toRefs, shallowReactive, shallowRef } from "vue";
+export default {
+  name: "Demo",
+  setup() {
+    //數據
+    // let person = shallowReactive({ //只考慮第一層數據的響應式
+    let person = reactive({
+      name: "張三",
+      age: 18,
+      job: {
+        j1: {
+          salary: 20,
+        },
+      },
+    });
+    let x = shallowRef({
+      y: 0,
+    });
+    console.log("******", x);
+
+    //返回一個對象（常用）
+    return {
+      x,
+      person,
+      ...toRefs(person),
+    };
+  },
+};
+</script>
