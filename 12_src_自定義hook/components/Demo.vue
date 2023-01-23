bdd97604bf6ca56a4310b8a0583e427f34d10041
r97221004 <r97221004@ntu.edu.tw>
2023-01-23 13:45:34 +0800

    add mod12

---
+++ 12_src_自定義hook/components/Demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>當前求和為：{{ sum }}</h2>
+  <button @click="sum++">點我+1</button>
+  <hr />
+  <h2>當前點擊時鼠標的坐標為：x：{{ point.x }}，y：{{ point.y }}</h2>
+</template>
+
+<script>
+import { ref } from "vue";
+import usePoint from "../hooks/usePoint";
+export default {
+  name: "Demo",
+  setup() {
+    //數據
+    let sum = ref(0);
+    let point = usePoint();
+
+    //返回一個對象（常用）
+    return { sum, point };
+  },
+};
+</script>
---
+++ 12_src_自定義hook/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>我是Test組件</h2>
+  <h2>當前點擊時鼠標的坐標為：x：{{ point.x }}，y：{{ point.y }}</h2>
+</template>
+
+<script>
+import usePoint from "../hooks/usePoint";
+export default {
+  name: "Test",
+  setup() {
+    const point = usePoint();
+    return { point };
+  },
+};
+</script>
