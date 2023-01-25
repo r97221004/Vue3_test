e766328b41480fa97f03688a526fda0027a6f251
r97221004 <r97221004@ntu.edu.tw>
2023-01-25 14:38:15 +0800

    add mod18

---
+++ 18_src_provide與inject/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <h3>我是App組件（祖），{{ name }}--{{ price }}</h3>
+    <Child />
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs, provide } from "vue";
+import Child from "./components/Child.vue";
+export default {
+  name: "App",
+  components: { Child },
+  setup() {
+    let car = reactive({ name: "奔馳", price: "40W" });
+    provide("car", car); //給自己的後代組件傳遞數據
+    return { ...toRefs(car) };
+  },
+};
+</script>
+
+<style>
+.app {
+  background-color: gray;
+  padding: 10px;
+}
+</style>
---
+++ 18_src_provide與inject/components/Son.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="son">
+    <h3>我是Son组件（孫），{{ car.name }}--{{ car.price }}</h3>
+  </div>
+</template>
+
+<script>
+import { inject } from "vue";
+export default {
+  name: "Son",
+  setup() {
+    let car = inject("car");
+    return { car };
+  },
+};
+</script>
+
+<style>
+.son {
+  background-color: orange;
+  padding: 10px;
+}
+</style>
